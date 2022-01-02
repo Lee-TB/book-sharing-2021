1b0be455299f0ddd9f397f785909bfa5d7a44f89
Lee-TB <86998194+Lee-TB@users.noreply.github.com>
2022-01-02 22:56:48 +0700

    Add footer: github info

--- layouts/footer.php
+++ layouts/footer.php
@@ -5,17 +5,19 @@
                 <div class="contact">
                     <div class="contact__facebook">
                         <a href="https://www.facebook.com/profile.php?id=100007547715762">
-                        <i class="fab fa-facebook"></i>
-                        Trần Bùi Lý Đức
-                    </a>
+                            <i class="fab fa-facebook"></i>
+                            Trần Bùi Lý Đức
+                        </a>
+                    </div>
+                    <div class="contact__github">
+                        <a href="https://github.com/Lee-TB">
+                            <i class="fab fa-github"></i>
+                            https://github.com/Lee-TB
+                        </a>
                     </div>
                     <div class="contact__gmail">
                         <i class="far fa-envelope"></i>
                         tblyduc2412@gmail.com
-                    </div>
-                    <div class="contact__phone">
-                        <i class="fas fa-phone"></i>
-                        0337611246
                     </div>
                 </div>
             </div>
